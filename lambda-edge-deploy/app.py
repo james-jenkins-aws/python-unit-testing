4cd5661402fb0c07376d4033bb438d7d7f10ca48
James Jenkins <jdjenk@amazon.com>
2023-03-13 14:43:22 -0600

    add lambda@edge code

--- lambda-edge-deploy/app.py
+++ lambda-edge-deploy/app.py
@@ -1,28 +1,42 @@
-#!/usr/bin/env python3
-import os
+from aws_cdk import (
+    aws_cloudfront as cloudfront,
+    aws_lambda as _lambda,
+    aws_iam as iam,
+    core,
+)
 
-import aws_cdk as cdk
+class LambdaEdgeStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-from lambda_edge_deploy.lambda_edge_deploy_stack import LambdaEdgeDeployStack
+        # Define Lambda function
+        lambda_fn = _lambda.Function(
+            self,
+            "MyLambdaEdgeFunction",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.asset("lambda_edge_function"),
+            handler="index.handler",
+        )
 
+        # Add permission to Lambda function to be invoked by CloudFront
+        lambda_fn.add_to_role_policy(
+            iam.PolicyStatement(
+                actions=["lambda:InvokeFunction"],
+                resources=[lambda_fn.function_arn],
+                principals=[
+                    iam.ServicePrincipal("edgelambda.amazonaws.com"),
+                    iam.ServicePrincipal("cloudfront.amazonaws.com"),
+                ],
+            )
+        )
 
-app = cdk.App()
-LambdaEdgeDeployStack(app, "LambdaEdgeDeployStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
-
-app.synth()
+        # Create CloudFront distribution with Lambda@Edge function
+        cloudfront.Distribution(
+            self,
+            "MyDistribution",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=cloudfront.S3Origin("my-s3-bucket.s3.amazonaws.com"),
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                edge_lambdas=[cloudfront.EdgeLambda(lambda_version=lambda_fn.current_version)]
+            )
+        )
